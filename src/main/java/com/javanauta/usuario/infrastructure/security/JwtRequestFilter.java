26a6154dc531c4e9f1b0fe3b5e55d9a0fb00a839
Andre <andreluiz.miranda@outlook.com>
2026-01-27 21:20:36 -0300

    atualização para problema de dependência no gradle.yml

--- src/main/java/com/javanauta/usuario/infrastructure/security/JwtRequestFilter.java
+++ src/main/java/com/javanauta/usuario/infrastructure/security/JwtRequestFilter.java
@@ -54,7 +54,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 }
             }
         }
-
         // Continua a cadeia de filtros, permitindo que a requisição prossiga
         chain.doFilter(request, response);
     }
